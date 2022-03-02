93a8a585536f9a80738caedc020b1b2384d3bf88
Amaury S. Minaya R <hardminaya@hotmail.com>
2022-03-02 01:39:14 -0400

    First Init

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Vessla NCF",
+    "description" : "AddOns para el control de NCF Version 1.0.0",
+    "version" : "1.0.0",
+    "author" : "Amaury Minaya",
+    "category" : "Vessla",
+    "data": [
+        'views.xml',
+        'ir.model.access.csv'
+    ]
+}
